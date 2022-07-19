4eb7e7f39342e4db043541766ab6359a74e454d3
rs-duongnt1 <duongnt1@runsystem.net>
2022-07-19 14:13:24 +0700

    Udpate

--- src/resources/validation/components/ValidationDetailDialog.tsx
+++ src/resources/validation/components/ValidationDetailDialog.tsx
@@ -3,15 +3,15 @@ import Button from "@mui/material/Button";
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
 import DialogContent from "@mui/material/DialogContent";
-import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
+import ListItemText from "@mui/material/ListItemText";
 import {
   Chip,
+  Divider,
   FormControl,
   IconButton,
   InputLabel,
   MenuItem,
-  Paper,
   Select,
   SelectChangeEvent,
   Stack,
@@ -25,6 +25,9 @@ import Box from "@mui/material/Box";
 import { Checkbox, FormControlLabel, FormGroup } from "@mui/material";
 import { Add } from "@mui/icons-material";
 import ModeEditOutlineOutlinedIcon from "@mui/icons-material/ModeEditOutlineOutlined";
+import { nanoid } from "nanoid";
+import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
+import MessageOutlinedIcon from "@mui/icons-material/MessageOutlined";
 function a11yProps(index: number) {
   return {
     id: `vertical-tab-${index}`,
@@ -32,6 +35,12 @@ function a11yProps(index: number) {
   };
 }
 
+interface GroupRule {
+  id: string;
+  name: string;
+  rules: string[];
+  message: string | null;
+}
 interface ValidationDetailDialogProps {
   open: boolean;
   onClose?: Function;
@@ -45,23 +54,22 @@ export default function ValidationDetailDialog({
     setValue(newValue);
   };
 
-  const [moreRules, setMoreRules] = React.useState<string[]>([
-    "hiragana",
-    "natakana",
-    "natakana",
-    "natakana",
-    "natakana",
-    "natakana",
-    "natakana",
-    "natakana",
-    "natakana",
-    "natakana",
-    "natakana",
-    "natakana",
-    "natakana",
-    "natakana",
-    "natakana",
+  const [groupRules, setGroupRules] = React.useState<GroupRule[]>([
+    {
+      id: nanoid(),
+      name: "group rule 1",
+      rules: ["email", "katakana"],
+      message: null,
+    },
+    {
+      id: nanoid(),
+      name: "group rule 2",
+      rules: ["string", "katakana"],
+      message: null,
+    },
   ]);
+
+  const rules = ["email", "string", "katakana", "hirakana"];
 
   const handleClose = () => {
     if (typeof onClose === "function") {
@@ -69,17 +77,29 @@ export default function ValidationDetailDialog({
     }
   };
 
-  // const addMoreRule = () => {
-  //   setMoreRules((rules: any[]) => [...rules, 1]);
-  // };
-
-  const handleChangeRule = (event: SelectChangeEvent<typeof moreRules>) => {
+  const handleChangeGroupRule = (
+    event: SelectChangeEvent<string[]>,
+    rule: GroupRule
+  ) => {
     const {
       target: { value },
     } = event;
-    setMoreRules(
-      // On autofill we get a stringified value.
-      typeof value === "string" ? value.split(",") : value
+    const _rules = [...groupRules];
+    const index = _rules.findIndex((_rule) => _rule.id === rule.id);
+    _rules[index].rules = value as any;
+    setGroupRules(_rules);
+  };
+
+  const addGroupRule = () => {
+    setGroupRules((rules) => [
+      ...rules,
+      { id: nanoid(), name: "Untitled", rules: [], message: null },
+    ]);
+  };
+
+  const removeGroupRule = (groupRule: GroupRule) => {
+    setGroupRules((groupRules) =>
+      groupRules.filter((_groupRule) => _groupRule.id !== groupRule.id)
     );
   };
 
@@ -111,7 +131,7 @@ export default function ValidationDetailDialog({
                 <Tab label="gender" {...a11yProps(3)} />
               </Tabs>
             </Stack>
-            <Stack px="30px" spacing={3}>
+            <Stack px="30px" spacing={3} flex={1}>
               <Stack>
                 <Typography color="rgb(0 0 0 / 64%)">Field Name</Typography>
                 <TextField
@@ -138,52 +158,83 @@ export default function ValidationDetailDialog({
                   </Select>
                 </FormControl>
               </Stack>
-              <Stack direction="row">
-                <FormGroup>
-                  <FormControlLabel
-                    control={<Checkbox defaultChecked />}
-                    label="Required"
-                  />
-                </FormGroup>
-                <ModeEditOutlineOutlinedIcon />
-              </Stack>
               <Stack>
-                <Typography color="rgb(0 0 0 / 64%)">Length</Typography>
-                <Stack direction="row" spacing={1}>
-                  <TextField
-                    type="number"
-                    label="Max length"
-                    variant="standard"
-                    size="small"
-                    focused
-                    defaultValue={0}
-                  />
-                  <TextField
-                    type="number"
-                    label="Min length"
-                    variant="standard"
-                    size="small"
-                    focused
-                    defaultValue={10}
-                  />
+                <Typography variant="h6">Base</Typography>
+                <Divider />
+              </Stack>
+              <Stack spacing={2} px={2}>
+                <Stack
+                  direction="row"
+                  justifyContent="space-between"
+                  alignItems="center"
+                >
+                  <FormGroup>
+                    <FormControlLabel
+                      control={<Checkbox defaultChecked />}
+                      label="Required"
+                    />
+                  </FormGroup>
+                  <Stack direction="row">
+                    <IconButton color="error">
+                      <MessageOutlinedIcon />
+                    </IconButton>
+                  </Stack>
+                </Stack>
+                <Stack>
+                  <Typography color="rgb(0 0 0 / 64%)">Length</Typography>
+                  <Stack direction="row" justifyContent="space-between">
+                    <Stack direction="row" spacing={1}>
+                      <TextField
+                        type="number"
+                        label="Max length"
+                        variant="standard"
+                        size="small"
+                        focused
+                        defaultValue={0}
+                      />
+                      <TextField
+                        type="number"
+                        label="Min length"
+                        variant="standard"
+                        size="small"
+                        focused
+                        defaultValue={10}
+                      />
+                    </Stack>
+                    <Stack direction="row">
+                      <IconButton color="error">
+                        <MessageOutlinedIcon />
+                      </IconButton>
+                    </Stack>
+                  </Stack>
                 </Stack>
               </Stack>
               <Stack>
-                <Typography color="rgb(0 0 0 / 64%)" mb={2}>
-                  More rules
-                </Typography>
+                <Typography variant="h6">Custom</Typography>
+                <Divider />
+              </Stack>
+              <Stack spacing={2} px={2}>
                 <Stack spacing={2}>
-                  {moreRules.map((rule) => (
-                    <Stack direction="row" key={rule}>
+                  {groupRules.length === 0 && (
+                    <Typography fontStyle="italic" color="gray" fontSize="13px" textAlign="center">Empty List</Typography>
+                  )}
+                  {groupRules.map((groupRule) => (
+                    <Stack
+                      direction="row"
+                      key={groupRule.id}
+                      alignItems="flex-end"
+                      spacing={1}
+                    >
                       <FormControl fullWidth size="small">
-                        <InputLabel id="demo-select-small">My rule</InputLabel>
+                        <InputLabel id="demo-select-small">
+                          {groupRule.name}
+                        </InputLabel>
                         <Select
                           multiple
                           variant="standard"
                           labelId="demo-select-small"
                           id="demo-select-small"
-                          value={moreRules}
-                          label="Age"
+                          value={groupRule.rules}
                           renderValue={(selected) => (
                             <Box
                               sx={{
@@ -192,30 +243,48 @@ export default function ValidationDetailDialog({
                                 gap: 0.5,
                               }}
                             >
-                              {selected.map((value) => (
+                              {selected.map((value: string) => (
                                 <Chip key={value} label={value} />
                               ))}
                             </Box>
                           )}
-                          onChange={handleChangeRule}
+                          onChange={(e: SelectChangeEvent<string[]>) =>
+                            handleChangeGroupRule(e, groupRule)
+                          }
                         >
-                          <MenuItem value="katakana">Katakana</MenuItem>
-                          <MenuItem value="hirakana">Hirakana</MenuItem>
-                          <MenuItem value="email">Email</MenuItem>
+                          {rules.map((ruleName) => (
+                            <MenuItem value={ruleName} key={ruleName}>
+                              <Checkbox
+                                checked={groupRule.rules.indexOf(ruleName) > -1}
+                              />
+                              <ListItemText primary={ruleName} />
+                            </MenuItem>
+                          ))}
                         </Select>
                       </FormControl>
+                      <Stack direction="row">
+                        <IconButton
+                          color="error"
+                          onClick={() => removeGroupRule(groupRule)}
+                        >
+                          <DeleteOutlineOutlinedIcon />
+                        </IconButton>
+                        <IconButton color="error">
+                          <MessageOutlinedIcon />
+                        </IconButton>
+                      </Stack>
                     </Stack>
                   ))}
                 </Stack>
 
                 <Stack direction="row" spacing={1}>
-                  <IconButton
+                  <Button
+                    variant="contained"
+                    onClick={addGroupRule}
                     size="small"
-                    color="primary"
-                    // onClick={addMoreRule}
                   >
-                    <Add />
-                  </IconButton>
+                    Add
+                  </Button>
                 </Stack>
               </Stack>
             </Stack>
